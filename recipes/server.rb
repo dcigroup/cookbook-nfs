6939abaeb8b5c37f74e90ce868cc5be6070bf3a6
Brent Stephens <alanbrent@ligeia>
2014-03-08 16:23:20 -0500

    Not upstart for server

--- recipes/server.rb
+++ recipes/server.rb
@@ -27,12 +27,6 @@ end
 
 # Start nfs-server components
 service node['nfs']['service']['server'] do
-  case node['platform']
-  when "ubuntu"
-    if node["platform_version"].to_f >= 9.10
-      provider Chef::Provider::Service::Upstart
-    end
-  end
   action [ :start, :enable ]
   supports :status => true
 end
